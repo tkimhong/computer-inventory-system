fd50814f6f49f39be134fa52ba492715a5cf5673
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-04-17 01:23:58 +0700

    Add User model

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["Admin", "Technician"], required: true },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("User", userSchema);
