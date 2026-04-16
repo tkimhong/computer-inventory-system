2027b5f488d0b3c0ac554a4b0819b48c2e2c2404
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-04-17 01:24:43 +0700

    Add ApiKey model

---
+++ models/ApiKey.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const apiKeySchema = new mongoose.Schema(
+  {
+    hashedKey: { type: String, required: true },
+    label: { type: String },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("ApiKey", apiKeySchema);
