9d8b6e063df0127d49efb722f46a6b833acaf42b
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-04-28 11:37:12 +0700

    Add multer upload middleware

---
+++ middleware/upload.js
@@ -0,0 +1,12 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (request, file, cb) => (null, "uploads/"),
+  filename: (request, file, cb) => {
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, unique + path.extname(file.originalname));
+  },
+});
+
+module.exports = multer({ storage });
